053801e380bb5d07323386960a243cd12cc16185
JNicolasmm <jhorsmm1721@hotmail.com>
2023-05-03 19:19:27 -0500

    Componentes Card, Cards y SearchBar

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card(props) {
+  const {name, status, species, gender, origin, image, onClose} = props
+   return (
+      <div>
+         <img src = {image} alt='' />
+         <h2>Personaje: {name}</h2>
+         <h2>Status: {status}</h2>
+         <h2>Especie: {species}</h2>
+         <h2>Genero: {gender}</h2>
+         <h2>Origen: {origin}</h2>
+         <button onClick = {onClose}>X</button>
+      </div>
+   );
+}
